a858c023e265456c3b0f30135666ff78d3de7eac
shalitha alahakoon <79890761+ShalithaAlahakoon@users.noreply.github.com>
2022-04-21 17:57:59 +0530

    express async handler installed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,43 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
+
 
 //@desc register user
 //@route POST /api/user
 //@access public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async(req, res) => {
+
+    const { name, email, password, role } = req.body;
+
+    if(!name || !email || !password || !role) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please enter all fields'
+        });
+    }
+
     res.json({message: 'User registered'});
 }
-
+)
 //@desc Authenticate user
 //@route POST /api/user/login
 //@access public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.json({message: 'User login'});
 }
-
+)
 //@desc get user data
 //@route GET /api/user/getUser
 //@access public
 
-const getUser = (req, res) => {
+const getUser = asyncHandler( async(req, res) => {
     res.json({message: 'User data'});
 }
-
+)
 module.exports = {
     registerUser,
     loginUser,
